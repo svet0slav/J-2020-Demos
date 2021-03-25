bbbb5bfa03c05d1eb1bfee0b6d0d56acd161ac10
Svetoslav Ivanov <svetoslav.ivanov@company.com>
2021-03-25 11:49:21 +0200

    TaxCalculator C21 - Console application

---
+++ TaxCalculator.C21/TaxCalculator.C21.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SConsole = System.Console;
+using TaxCalculator.C21.Services;
+using TaxCalculator.C21.Common.Data;
+
+namespace TaxCalculator.C21.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SConsole.WriteLine("Hello, enter gross amount:");
+            var amountText = SConsole.ReadLine();
+            if (decimal.TryParse(amountText, out decimal amount))
+            {
+                var service = new TaxCalculatorService(new TaxCalculatorDefinitionPoCService());
+                var salary = new Salary() { GrossAmount = amount };
+                service.CalculateTax(salary);
+                SConsole.WriteLine($"Gross amount : {salary.GrossAmount:N2}");
+                SConsole.WriteLine($"The tax is {salary.TaxAmount:N2}");
+                if (salary.TaxExplanation != null)
+                {
+                    foreach(var item in salary.TaxExplanation)
+                    {
+                        SConsole.WriteLine($"includes {item.Key.Name} with tax {item.Value:N2}");
+                    }
+                }
+                SConsole.WriteLine($"Net amount : {salary.NetAmount}");
+            }
+
+            SConsole.WriteLine("Finished. Press ENTER to exit!");
+            SConsole.ReadLine();
+        }
+    }
+}
---
+++ TaxCalculator.C21/TaxCalculator.C21.Console/TaxCalculatorDefinitionPoCService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using TaxCalculator.C21.Common.Data;
+using TaxCalculator.C21.Services;
+
+namespace TaxCalculator.C21.Console
+{
+    public class TaxCalculatorDefinitionPoCService : ITaxCalculatorDefinitionService
+    {
+        public TaxDefinition GetTaxDefinition(DateTime moment)
+        {
+            //This is prove of concept. Hard-coded data.
+            var result = new TaxDefinition()
+            {
+                Id = 0,
+                //First and last day of the month
+                PeriodFrom = new DateTime(moment.Year, moment.Month, 1),
+                PeriodTo = new DateTime(moment.Year, moment.Month, moment.AddMonths(1).AddDays(-1).Day),
+                Definitions = new List<TaxDefinitionItem>()
+                {
+                    new TaxDefinitionItem()
+                    {
+                        Name = "Tax does not apply",
+                        FromAmountIncluding = 0m,
+                        UpToAmount = 1000m,
+                        BaseTaxAmount = 0,
+                        PercentAboveFrom = 0,
+                    },
+                    new TaxDefinitionItem()
+                    {
+                        Name = "Tax 10%",
+                        FromAmountIncluding = 1000m,
+                        UpToAmount = TaxDefinitionItem.MaxUpToAmount,
+                        BaseTaxAmount = 0m,
+                        PercentAboveFrom = 0.10m
+                    },
+                    new TaxDefinitionItem()
+                    {
+                        Name = "Social contribution",
+                        FromAmountIncluding = 1000m,
+                        UpToAmount = 3000m,
+                        BaseTaxAmount = 0m,
+                        PercentAboveFrom = 0.15m
+                    },
+                    // This definition adds social contribution for income above 3000 or the amount 1000-3000.
+                    new TaxDefinitionItem()
+                    {
+                        Name = "Social contribution for above 3000",
+                        FromAmountIncluding = 3000m,
+                        UpToAmount = TaxDefinitionItem.MaxUpToAmount,
+                        BaseTaxAmount = 300m,
+                        PercentAboveFrom = 0m
+                    }
+                }
+            };
+
+            return result;
+        }
+    }
+}
